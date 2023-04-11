b1dceea60760e218beb53a03b6ce8d4b11d12f13
Nadya <106408597+progNK@users.noreply.github.com>
2023-04-11 22:33:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.stats import ks_2samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1068869116 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
